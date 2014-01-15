17dc78b95d595fc07ff566724c55468038a83c2c
jpomez <julien.pomez@outlook.com>
2014-01-15 02:11:33 +0100

    BrainFuck interpreter first issue

---
+++ FSharpSandbox/BrainFuckInterpreter.fs
@@ -0,0 +1,27 @@
+﻿module BrainFuckInterpreter
+
+open System.Text
+ 
+let rec FindMatchingBracket (program : string) bracket cursor =
+    if program.[cursor] = bracket then cursor
+    else match bracket with
+        | ']' -> FindMatchingBracket program bracket (cursor + 1)
+        | '[' -> FindMatchingBracket program bracket (cursor - 1)
+        | _ -> failwith "Not a bracket"
+
+
+let rec Step (program : string) (tape : byte[]) cursor pointer = 
+    if program.Length <= 0 then failwith "The program is empty"
+    if cursor >= program.Length then ""
+    else
+        match program.[cursor] with
+            | '>' -> Step program tape (cursor + 1) (pointer + 1)
+            | '<' -> Step program tape (cursor + 1) (pointer - 1)
+            | '+' -> tape.[pointer] <- tape.[pointer] + 1uy; Step program tape (cursor + 1) pointer
+            | '-' -> tape.[pointer] <- tape.[pointer] - 1uy; Step program tape (cursor + 1) pointer
+            | '.' -> Encoding.UTF8.GetString(tape, pointer, 1) + Step program tape (cursor + 1) pointer
+            | '[' when tape.[pointer] <> 0uy -> Step program tape (cursor + 1) pointer
+            | '[' when tape.[pointer] = 0uy -> Step program tape (FindMatchingBracket program ']' cursor + 1) pointer
+            | ']' when tape.[pointer] <> 0uy -> Step program tape (FindMatchingBracket program '[' cursor + 1) pointer
+            | ']' when tape.[pointer] = 0uy -> Step program tape (cursor + 1) pointer
+            | _ -> ""
---
+++ FSharpSandbox/Program.fs
@@ -0,0 +1,11 @@
+﻿module EntryPoint
+
+open System
+open BrainFuckInterpreter
+
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine(Step "[-]>[-]<>+++++++[<+++++++>-]<+++.--." (Array.create 640000 0uy) 0 0)
+    Console.WriteLine(Step "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>." (Array.create 640000 0uy) 0 0)
+    Console.ReadLine()
+    0 // return an integer exit code
